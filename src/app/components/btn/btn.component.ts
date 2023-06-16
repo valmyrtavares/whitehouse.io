a1703a003d512086d21785e94fd301e45360554d
Valmyr Tavares <valmyrtavares71@gmail.com>
2023-06-16 18:00:02 -0300

    header e botão de promoções feito

---
+++ src/app/components/btn/btn.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'taba-btn',
+  templateUrl: './btn.component.html',
+  styleUrls: ['./btn.component.scss'],
+})
+export class BtnComponent {}
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent {
+  imagem: string =
+    'https://drive.google.com/uc?export=download&id=15ssu7Z0gGjSnbBtdgoKKLkyVBT8Tmu3-';
+}
