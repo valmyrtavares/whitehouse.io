17f53c0b036f290e7f9e09ac35fb52a18bf30166
valmyrtavares <valmyrtavares71@gmail.com>
2024-06-05 14:58:39 -0300

    updated angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AdminModule } from './view/admin/admin.module';
 import { ComponentModule } from './components/component.module';
 import { EnvironmentImagesModule } from './view/admin/environment-images/environment-images.module';
 import { ViewModule } from './view/view.module';
 import { AdminGuardService } from './mock/admin-guard.service';
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    AdminModule,
-    ComponentModule,
-    EnvironmentImagesModule,
-    ViewModule,
-  ],
-  providers: [AdminGuardService],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        AdminModule,
+        ComponentModule,
+        EnvironmentImagesModule,
+        ViewModule], providers: [AdminGuardService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
